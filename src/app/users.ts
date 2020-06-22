c002cb45deca831f61194cb36a2916c6c14e0fc8
George Okumu <gokumu12@gmail.com>
2020-06-23 02:47:26 +0300

    declared user constructor properties found in github

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,12 @@
 export class Users {
+    constructor(
+        public login: string,
+        public avatar_url: string
+        public followers_url: string,
+        public following_url: string,
+        public repos_url: string,
+        public name: string,
+        public repo_url: string,
+        public createdAt: Date
+      ) {}
 }
